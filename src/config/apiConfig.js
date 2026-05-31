fc367d9ebafc4aaa4576edb6081cbc0f0987fcf4
Arpita <arrpita0019@gmail.com>
2026-05-31 17:42:31 +0530

    Env Variable changed

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,3 +1,4 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+export const API_HTTP_URL = import.meta.env.VITE_API_HTTP_URL || 'http://api.mynonveg.com/api';
 
 export default API_BASE_URL;
